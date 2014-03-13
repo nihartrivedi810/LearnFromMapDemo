77bb51acc155ac972b06c4c8d18065fb432c8da3
nihartrivedi810 <nihar.trivedi888@gmail.com>
2014-03-13 07:03:22 +0530

    added map and checked the address of the currnet marker position to match the answer

--- LearnFromMap/src/com/example/learnfrommap/MainActivity.java
+++ LearnFromMap/src/com/example/learnfrommap/MainActivity.java
@@ -1,23 +1,164 @@
 package com.example.learnfrommap;
 
-import android.os.Bundle;
+import java.io.IOException;
+import java.security.Provider;
+import java.util.List;
+import java.util.Locale;
+import java.util.concurrent.ExecutionException;
+
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	GoogleMap map;
+	MapFragment mapFragment;
+	Button check, start;
+	TextView question;
+	Location location;
+	String[] questions,answers;
+	static int number=0;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		mapFragment = (MapFragment) getFragmentManager().findFragmentById(
+				R.id.map);
+		check = (Button) findViewById(R.id.check_button);
+		start = (Button) findViewById(R.id.start_button);
+		question = (TextView) findViewById(R.id.question);
+		map = mapFragment.getMap();
+		location=new Location("");
+		CameraUpdate center = CameraUpdateFactory.newLatLng(new LatLng(
+				22.006891, 78.618164));
+		CameraUpdate zoom = CameraUpdateFactory.zoomTo(4);
+		questions=new String[5];
+		questions[0]="Ahmedabad";
+		questions[1]="Rajkot";
+		questions[2]="Gandhinagar";
+		answers=new String[5];
+		map.moveCamera(center);
+		map.animateCamera(zoom);
+		map.setOnMapClickListener(new OnMapClickListener() {
+
+			@Override
+			public void onMapClick(LatLng arg0) {
+				// TODO Auto-generated method stub
+				map.clear();
+				map.addMarker(new MarkerOptions().position(arg0)
+						.draggable(true));
+				location.setLatitude(arg0.latitude);
+				location.setLongitude(arg0.longitude);
+			}
+		});
+		start.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				check.setVisibility(View.VISIBLE);
+				start.setVisibility(View.GONE);
+				question.setVisibility(View.VISIBLE);
+				question.setText(questions[number]);
+			}
+		});
+		check.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				if(checkAnswer()==true)
+					number++;
+				question.setText(questions[number]);
+			}
+		});
+	}
+
+	private boolean checkAnswer() {
+		GetAddressTask addressTask = new GetAddressTask(MainActivity.this);
+		try {
+			if(addressTask.execute(location).get().equals("Ahmedabad"))
+				Toast.makeText(getApplicationContext(), "correct answer", Toast.LENGTH_SHORT).show();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return true;
+	}
+
+	private class GetAddressTask extends AsyncTask<Location, Void, String> {
+		Context mContext;
+
+		public GetAddressTask(Context context) {
+			super();
+			mContext = context;
+		}
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+		@Override
+		protected String doInBackground(Location... params) {
+			Geocoder geocoder = new Geocoder(mContext, Locale.getDefault());
+			// Get the current location from the input parameter list
+			Location loc = params[0];
+			// Create a list to contain the result address
+			List<Address> addresses = null;
+			try {
+			
+				addresses = geocoder.getFromLocation(loc.getLatitude(),
+						loc.getLongitude(), 1);
+			} catch (IOException e1) {
+				Log.e("LocationSampleActivity",
+						"IO Exception in getFromLocation()");
+				e1.printStackTrace();
+				return ("IO Exception trying to get address");
+			} catch (IllegalArgumentException e2) {
+				// Error message to post in the log
+				String errorString = "Illegal arguments "
+						+ Double.toString(loc.getLatitude()) + " , "
+						+ Double.toString(loc.getLongitude())
+						+ " passed to address service";
+				Log.e("LocationSampleActivity", errorString);
+				e2.printStackTrace();
+				return errorString;
+			}
+			// If the reverse geocode returned an address
+			if (addresses != null && addresses.size() > 0) {
+				// Get the first address
+				Address address = addresses.get(0);
+				
+				return address.getLocality();
+			} else {
+				return "No address found";
+			}
+		}
+		@Override
+		protected void onPostExecute(String result) {
+			// TODO Auto-generated method stub
+			super.onPostExecute(result);
+			//question.setText(result);
+		}
+	}
 }
